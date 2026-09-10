0a282a72559f82b01da8a6586d032e5874a2b763
LakhMarko <lah.marko.vas@gmail.com>
2026-09-10 23:09:40 +0300

    Add files via upload

---
+++ laba1.py
@@ -0,0 +1,45 @@
+#ультра база
+print("Hello World")
+
+#змінні
+word = "Hello World"
+print(word)
+solid_number = 1
+print(solid_number)
+liquid_number = 1.5
+print(liquid_number)
+pravda = True
+print(pravda)
+lst=["Яблуко","Груша","Банан"]
+print(lst)
+print(lst[0])
+my_tuple = (1,2,3)
+print(my_tuple)
+my_dict = {1: 2, 2: 3}
+print(my_dict)
+my_set = {1, 2, 2, 3, 3, 3}
+print(my_set)
+#визначення типу
+print(type(my_set))
+
+#оператори
+a = 42
+b = 3.2
+print(a + b)
+print(a + b == 3)
+print(a + b == 45.2)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+''' 
+крутий коментарь
+а далі ввід даних 
+(від користувача)
+та оператори з текстом
+'''
+what_user_say = input("Введіть і воно повториться 3 рази: ")
+print(what_user_say * 3)
